c111df314e38745c9c50763ddd99920ef17a8fc4
Dagger <polgomez624@gmail.com>
2025-10-16 21:52:14 +0200

    Direction of mario

--- js/entities/mario.js
+++ js/entities/mario.js
@@ -16,12 +16,28 @@ export class Mario extends Entity {
     this.jumpSSy = 10;
 
     this.size = 2;
+
+    this.renderDir = 1;
+    this.frameRowCount = [1,3,1]
+    this.frameRow = 0
+
+    this.curFrame = 0;
   }
 
   update(deltaTime) {
     this.vx = 0;
-    if (isKeyDown("ArrowRight")) this.vx = MOVE_SPEED;
-    if (isKeyDown("ArrowLeft")) this.vx = -MOVE_SPEED;
+    if (isKeyDown("ArrowRight"))
+      { 
+        this.vx = MOVE_SPEED;
+        this.renderDir = 1;
+        this.frameRow = 1;
+      }
+    if (isKeyDown("ArrowLeft"))
+      {
+       this.vx = -MOVE_SPEED;
+       this.renderDir = -1;
+       this.frameRow = 1;
+    }
 
     if (isKeyDown("ArrowUp") && this.onGround) {
       this.vy = JUMP_FORCE;
@@ -49,25 +65,66 @@ export class Mario extends Entity {
 
     img.src = "./media/BlocksSpriteSheet.png"
 
-    if(this.isJumping){
-    ctx.drawImage(img,
-      this.jumpSSx*160,this.jumpSSy*160,
-      160,320,
-      this.x * RENDER_SCALE,
-      this.y * RENDER_SCALE,
-      this.width * RENDER_SCALE,
-      this.height * RENDER_SCALE
-    );
+    if (this.isJumping) {
+      if (this.renderDir == -1) {
+        ctx.save();
+        ctx.translate(
+          (this.x + this.width) * RENDER_SCALE, 
+          this.y * RENDER_SCALE
+        );
+        ctx.scale(-1, 1);
+        ctx.drawImage(
+          img,
+          this.jumpSSx * 160, this.jumpSSy * 160,  // source x, y for jumping sprite
+          160, 320,                                // source width, height
+          0, 0,                                    // flipped draw position
+          this.width * RENDER_SCALE,
+          this.height * RENDER_SCALE
+        );
+        ctx.restore();
+      } else {
+        ctx.drawImage(
+          img,
+          this.jumpSSx * 160, this.jumpSSy * 160,
+          160, 320,
+          this.x * RENDER_SCALE,
+          this.y * RENDER_SCALE,
+          this.width * RENDER_SCALE,
+          this.height * RENDER_SCALE
+        );
+      }
     }
+
     else{
-      ctx.drawImage(img,
-      this.SSx*160,this.SSy*160,
-      160,320,
-      this.x * RENDER_SCALE,
-      this.y * RENDER_SCALE,
-      this.width * RENDER_SCALE,
-      this.height * RENDER_SCALE
-    );
+      if(this.renderDir == -1){
+        ctx.save();
+        ctx.translate(
+          (this.x + this.width) * RENDER_SCALE, 
+          this.y * RENDER_SCALE
+        );
+        ctx.scale(-1, 1);
+        ctx.drawImage(
+          img,
+          this.SSx * 160, this.SSy * 160,   // source x, y
+          160, 320,                         // source width, height
+          0, 0,                             // destination x, y (we've already translated)
+          this.width * RENDER_SCALE,
+          this.height * RENDER_SCALE
+        );
+
+        ctx.restore(); // Restore original transform
+      }
+      else{
+        ctx.drawImage(img,
+        this.SSx*160,this.SSy*160,
+        160,320,
+        this.x * RENDER_SCALE,
+        this.y * RENDER_SCALE,
+        this.width * RENDER_SCALE,
+        this.height * RENDER_SCALE
+      );
+    }
+    
     }
   }
 
